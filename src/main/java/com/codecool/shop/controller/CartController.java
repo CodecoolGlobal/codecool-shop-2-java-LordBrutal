6c5454c6f0298009519c02be688a1919b91821e9
predavies <iamosaretin@gmail.com>
2021-12-01 15:22:18 +0100

    Add: create and send json string to front end

--- src/main/java/com/codecool/shop/controller/CartController.java
+++ src/main/java/com/codecool/shop/controller/CartController.java
@@ -8,22 +8,29 @@ import com.codecool.shop.model.Product;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.List;
+
 
 @WebServlet(urlPatterns = {"/cart"})
 public class CartController extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+
+
         String items = req.getParameter("cart");
         System.out.println(items);
+
+        PrintWriter out = resp.getWriter();
 
         ProductDao productDataStore = ProductDaoMem.getInstance();
 
@@ -31,9 +38,43 @@ public class CartController extends HttpServlet {
         Type merchantListType = new TypeToken<List<CartItem>>() {}.getType();
         List<CartItem> cartItems = gson.fromJson(String.valueOf(items), merchantListType);
 
+        StringBuilder json = new StringBuilder(); //TODO: Refactor this
+        json.append("[");
         for (CartItem cartItem : cartItems) {
-            System.out.println(cartItem.getId());
             Product product = productDataStore.find(cartItem.getId());
+            System.out.println(product.getPrice());
+            System.out.println(cartItem.getPiece());
+            json.append("{");
+            json.append("\"");
+            json.append("id");
+            json.append("\":");
+            json.append("\"");
+            json.append(product.getId());
+            json.append("\"");
+            json.append(",");
+            json.append("\"");
+            json.append("name");
+            json.append("\":");
+            json.append("\"");
+            json.append(product.getName());
+            json.append("\"");
+            json.append(",");
+            json.append("\"");
+            json.append("price");
+            json.append("\":");
+            json.append("\"");
+            json.append(product.getPrice());
+            json.append("\"");
+            json.append("}");
+            json.append(",");
         }
+
+
+        json.append("]");
+        json.deleteCharAt(json.length() -2);
+
+        out.println(json);
+
+
     }
 }
