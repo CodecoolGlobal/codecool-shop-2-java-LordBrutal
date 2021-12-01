50279e83c50e611882ee2b5e385c0a9a22622579
predavies <iamosaretin@gmail.com>
2021-12-01 14:12:25 +0100

    Add: CartItem.java

---
+++ src/main/java/com/codecool/shop/model/CartItem.java
@@ -0,0 +1,22 @@
+package com.codecool.shop.model;
+
+public class CartItem {
+    private int id;
+    private int piece;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getPiece() {
+        return piece;
+    }
+
+    public void setPiece(int piece) {
+        this.piece = piece;
+    }
+}
