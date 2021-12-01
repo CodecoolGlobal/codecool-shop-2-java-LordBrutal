97202d27d2b21bbb0fc11ca7037087a5f0c860ad
LordBrutal <bogar.jozsef@gmail.com>
2021-12-01 14:20:27 +0100

    Add: CreditCard class and its fields

---
+++ src/main/java/com/codecool/shop/model/paymentmodel/CreditCard.java
@@ -0,0 +1,10 @@
+package com.codecool.shop.model.paymentmodel;
+
+public class CreditCard {
+
+    private long cardNumber;
+    private String cardHolder;
+    private byte expYear;
+    private byte expMonth;
+    private short cvv;
+}
