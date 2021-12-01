fa9d72c78afcd50c075d7acd95e036cace1afc4a
predavies <iamosaretin@gmail.com>
2021-12-01 14:13:32 +0100

    Add: CartController.java get session data

--- src/main/java/com/codecool/shop/controller/CartController.java
+++ src/main/java/com/codecool/shop/controller/CartController.java
@@ -1,9 +1,12 @@
 package com.codecool.shop.controller;
 
 
-import com.codecool.shop.config.TemplateEngineUtil;
-import org.thymeleaf.TemplateEngine;
-import org.thymeleaf.context.WebContext;
+import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.implementation.ProductDaoMem;
+import com.codecool.shop.model.CartItem;
+import com.codecool.shop.model.Product;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,17 +14,26 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.List;
 
 @WebServlet(urlPatterns = {"/cart"})
 public class CartController extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
         String items = req.getParameter("cart");
         System.out.println(items);
-        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
-        WebContext context = new WebContext(req, resp, req.getServletContext());
 
-//        engine.process("/payment/payment.html", context, resp.getWriter());
+        ProductDao productDataStore = ProductDaoMem.getInstance();
+
+        Gson gson = new Gson();
+        Type merchantListType = new TypeToken<List<CartItem>>() {}.getType();
+        List<CartItem> cartItems = gson.fromJson(String.valueOf(items), merchantListType);
+
+        for (CartItem cartItem : cartItems) {
+            System.out.println(cartItem.getId());
+            Product product = productDataStore.find(cartItem.getId());
+        }
     }
 }
