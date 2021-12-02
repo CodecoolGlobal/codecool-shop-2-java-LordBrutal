8453764bbd5b49a4b9c221edc393f99f94e0260d
LordBrutal <bogar.jozsef@gmail.com>
2021-12-02 15:06:42 +0100

    Add: PayPalAccountDaoMem its fields and methods

---
+++ src/main/java/com/codecool/shop/dao/implementation/PayPalAccountDaoMem.java
@@ -0,0 +1,39 @@
+package com.codecool.shop.dao.implementation;
+
+import com.codecool.shop.dao.PayPalAccountDao;
+import com.codecool.shop.model.paymentmodel.PayPalAccount;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class PayPalAccountDaoMem implements PayPalAccountDao {
+
+    private List<PayPalAccount> accountList = new ArrayList<>();
+    private static PayPalAccountDaoMem instance = null;
+
+    private PayPalAccountDaoMem() {
+    }
+
+    public static PayPalAccountDaoMem getInstance() {
+        if (instance == null) {
+            instance = new PayPalAccountDaoMem();
+        }
+        return instance;
+    }
+
+
+    @Override
+    public void add(PayPalAccount account) {
+        accountList.add(account);
+    }
+
+    @Override
+    public PayPalAccount findAccount(String userName) {
+        return accountList
+                .stream()
+                .filter(account -> Objects.equals(account.getUsername(), userName))
+                .findFirst()
+                .orElse(null);
+    }
+}
