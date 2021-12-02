faa0796b9c399ebdc55e695df73804b951d96178
LordBrutal <bogar.jozsef@gmail.com>
2021-12-02 12:14:06 +0100

    Refactor: remove some unnecessary br tag

--- src/main/webapp/static/js/payment.js
+++ src/main/webapp/static/js/payment.js
@@ -14,17 +14,17 @@ function cardBtnClickEventHandler() {
     cardModalContent.innerHTML = `<span class="close">&times;</span>
             <div class="card_credentials">
                 <form action="#">
-                <label for="card_number">Card number</label><br>
-                <input type="number" id="card_number" placeholder="1234567890" required><br>
-                <label for="card_holder">Card holder</label><br>
-                <input type="text" id="card_holder" placeholder="John Doe" required><br>
-                <label for="expiry_date">Expiration date</label><br>
+                <label for="card_number">Card number</label>
+                <input type="number" id="card_number" placeholder="1234567890" required>
+                <label for="card_holder">Card holder</label>
+                <input type="text" id="card_holder" placeholder="John Doe" required>
+                <label for="expiry_date">Expiration date</label>
                 <div class="exp-wrapper" id="expiry_date">
                     <input autocomplete="off" class="exp" id="month" maxlength="2" pattern="[0-9]*" inputmode="numerical" placeholder="MM" type="text" data-pattern-validate />
                     <input autocomplete="off" class="exp" id="year" maxlength="2" pattern="[0-9]*" inputmode="numerical" placeholder="YY" type="text" data-pattern-validate />
                 </div>
-                <label for="cvv_code">CVV code</label><br>
-                <input id="cvv_code" type="password" required><br>
+                <label for="cvv_code">CVV code</label>
+                <input id="cvv_code" type="password" required>
                 <button type="button" id="submit_card">Submit</button>
                 </form>
 
