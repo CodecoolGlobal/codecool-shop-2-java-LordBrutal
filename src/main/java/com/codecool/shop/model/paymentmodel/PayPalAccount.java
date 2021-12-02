bb13d9e9754abc95d64d5964719bae0bdc16f6a3
LordBrutal <bogar.jozsef@gmail.com>
2021-12-02 12:14:54 +0100

    Add: constructor and getters for fields

--- src/main/java/com/codecool/shop/model/paymentmodel/PayPalAccount.java
+++ src/main/java/com/codecool/shop/model/paymentmodel/PayPalAccount.java
@@ -4,4 +4,17 @@ public class PayPalAccount {
 
     private String username;
     private String password;
+
+    public PayPalAccount(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
