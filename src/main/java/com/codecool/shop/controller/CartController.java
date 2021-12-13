43359ac4ae4f8dab784bd318dc7a681cd9ab0460
akos996 <balogh.akos.antal@gmail.com>
2021-12-13 12:21:16 +0100

    Refactor: removed unneeded imports in CartController

--- src/main/java/com/codecool/shop/controller/CartController.java
+++ src/main/java/com/codecool/shop/controller/CartController.java
@@ -4,22 +4,14 @@ package com.codecool.shop.controller;
 import com.codecool.shop.dao.ProductDao;
 import com.codecool.shop.dao.implementation.OrderDaoMem;
 import com.codecool.shop.dao.implementation.ProductDaoMem;
-import com.codecool.shop.model.CartItem;
-import com.codecool.shop.model.Product;
 import com.codecool.shop.service.CartService;
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.lang.reflect.Type;
-import java.util.ArrayList;
-import java.util.List;
 
 
 @WebServlet(urlPatterns = {"/cart"})
