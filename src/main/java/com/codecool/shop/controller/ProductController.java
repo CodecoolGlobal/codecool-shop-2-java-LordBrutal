4565a2175ccad0935e948ef47244d7eb8b6aeed9
akos996 <balogh.akos.antal@gmail.com>
2021-12-15 14:48:40 +0100

    Test: implementing jdbc use

--- src/main/java/com/codecool/shop/controller/ProductController.java
+++ src/main/java/com/codecool/shop/controller/ProductController.java
@@ -3,9 +3,7 @@ package com.codecool.shop.controller;
 import com.codecool.shop.dao.ProductCategoryDao;
 import com.codecool.shop.dao.ProductDao;
 import com.codecool.shop.dao.SupplierDao;
-import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
-import com.codecool.shop.dao.implementation.ProductDaoMem;
-import com.codecool.shop.dao.implementation.SupplierDaoMem;
+import com.codecool.shop.dao.implementation.*;
 import com.codecool.shop.model.Product;
 import com.codecool.shop.service.ProductService;
 import com.codecool.shop.config.TemplateEngineUtil;
@@ -16,6 +14,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 @WebServlet(urlPatterns = {"/"})
@@ -23,16 +22,25 @@ public class ProductController extends ServletBaseModel {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        ProductDao productDataStore = ProductDaoMem.getInstance(db);
-        ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance(db);
-        ProductService productService = new ProductService(productDataStore,productCategoryDataStore);
-        SupplierDao supplierDataStore = SupplierDaoMem.getInstance(db);
-
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
         WebContext context = new WebContext(req, resp, req.getServletContext());
         String categoryParameter = req.getParameter("categories");
         String supplyParameter = req.getParameter("supplier");
-        List<Product> products = productService.filterProducts(categoryParameter, supplyParameter, supplierDataStore);
+
+        ProductDao productDataStore = ProductDaoMem.getInstance();
+        ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
+        SupplierDao supplierDataStore = SupplierDaoMem.getInstance();
+        ProductService productService;
+        List<Product> products = new ArrayList<>();
+
+        if(true) {
+            productDataStore = ProductDaoJdbc.getInstance(db);
+            productCategoryDataStore = ProductCategoryDaoJdbc.getInstance(db);
+            supplierDataStore = SupplierDaoJdbc.getInstance(db);
+        }
+
+        productService = new ProductService(productDataStore,productCategoryDataStore);
+        products = productService.filterProducts(categoryParameter, supplyParameter, supplierDataStore);
 
         context.setVariable("products", products);
         context.setVariable("categories", productCategoryDataStore.getAll());
