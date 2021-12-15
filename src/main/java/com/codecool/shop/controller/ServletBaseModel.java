a32a8a2b31ad33549705027be3a66a959557b4e6
akos996 <balogh.akos.antal@gmail.com>
2021-12-15 17:27:41 +0100

    Add: connection properties to servlet

--- src/main/java/com/codecool/shop/controller/ServletBaseModel.java
+++ src/main/java/com/codecool/shop/controller/ServletBaseModel.java
@@ -4,16 +4,32 @@ import com.codecool.shop.dao.implementation.DatabaseManager;
 
 import javax.servlet.http.HttpServlet;
 import javax.sql.DataSource;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.SQLException;
+import java.util.Objects;
+import java.util.Properties;
 
 public class ServletBaseModel extends HttpServlet {
+
     protected DataSource db;
 
     {
         try {
-            db = new DatabaseManager().connect();
-        } catch (SQLException e) {
+            db = new DatabaseManager().connect(getConnectionProperties());
+        } catch (SQLException | IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public ServletBaseModel() {
+    }
+
+    protected Properties getConnectionProperties() throws IOException {
+        String rootPath = Objects.requireNonNull(Thread.currentThread().getContextClassLoader().getResource("")).getPath();
+        String appConfigPath = rootPath + "connection.properties";
+        Properties appProps = new Properties();
+        appProps.load(new FileInputStream(appConfigPath));
+        return appProps;
     }
 }
