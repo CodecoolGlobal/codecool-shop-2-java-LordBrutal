69e74ff27710abd2d008ff583de923fdda34bb01
akos996 <balogh.akos.antal@gmail.com>
2021-12-15 12:32:29 +0100

    Add: find and getAll for ProductCategory database use

--- src/main/java/com/codecool/shop/dao/implementation/ProductCategoryDaoJdbc.java
+++ src/main/java/com/codecool/shop/dao/implementation/ProductCategoryDaoJdbc.java
@@ -2,7 +2,11 @@ package com.codecool.shop.dao.implementation;
 
 import com.codecool.shop.dao.ProductCategoryDao;
 import com.codecool.shop.model.ProductCategory;
-
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,37 +14,61 @@ public class ProductCategoryDaoJdbc implements ProductCategoryDao {
 
     private List<ProductCategory> data = new ArrayList<>();
     private static ProductCategoryDaoJdbc instance = null;
+    private DataSource dataSource;
 
     /* A private Constructor prevents any other class from instantiating.
      */
-    private ProductCategoryDaoJdbc() {
+    private ProductCategoryDaoJdbc(DataSource dataSource) {
+        this.dataSource = dataSource;
     }
 
-    public static ProductCategoryDaoJdbc getInstance() {
+    public static ProductCategoryDaoJdbc getInstance(DataSource dataSource) {
         if (instance == null) {
-            instance = new ProductCategoryDaoJdbc();
+            instance = new ProductCategoryDaoJdbc(dataSource);
         }
         return instance;
     }
 
     @Override
     public void add(ProductCategory category) {
-        category.setId(data.size() + 1);
-        data.add(category);
     }
 
     @Override
     public ProductCategory find(int id) {
-        return data.stream().filter(t -> t.getId() == id).findFirst().orElse(null);
+        try (Connection conn = dataSource.getConnection()) {
+            String sql = "SELECT id, name, department, description FROM category c " +
+                    "WHERE c.id = ?";
+            PreparedStatement st = conn.prepareStatement(sql);
+            st.setInt(1, id);
+            ResultSet rs = conn.createStatement().executeQuery(sql);
+            if (!rs.next()) {
+                return null;
+            }
+            return new ProductCategory(rs.getString(2), rs.getString(3), rs.getString(4));
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void remove(int id) {
-        data.remove(find(id));
     }
 
     @Override
     public List<ProductCategory> getAll() {
-        return data;
+        try (Connection conn = dataSource.getConnection()) {
+            String sql = "SELECT p.id, p.name, price, currency, p.description, c.name, c.description, c.department, s.name, s.description FROM product p " +
+                    "LEFT JOIN category c ON p.category_id = c.id " +
+                    "LEFT JOIN supplier s on p.supplier_id = s.id";
+            ResultSet rs = conn.createStatement().executeQuery(sql);
+            List<ProductCategory> result = new ArrayList<>();
+            while (rs.next()) {
+                ProductCategory productCategory = new ProductCategory(rs.getString(2), rs.getString(3), rs.getString(4));
+                result.add(productCategory);
+            }
+            return result;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
