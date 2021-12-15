54d02f8bf8d209864f0a7acb4f4c7d68f53310e8
akos996 <balogh.akos.antal@gmail.com>
2021-12-15 11:59:22 +0100

    Add: implemented getter sql queries

--- src/main/java/com/codecool/shop/dao/implementation/ProductDaoJdbc.java
+++ src/main/java/com/codecool/shop/dao/implementation/ProductDaoJdbc.java
@@ -9,6 +9,7 @@ import jdk.jfr.Category;
 import javax.sql.DataSource;
 import java.math.BigDecimal;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -39,7 +40,21 @@ public class ProductDaoJdbc implements ProductDao {
 
     @Override
     public Product find(int id) {
-        return data.stream().filter(t -> t.getId() == id).findFirst().orElse(null);
+        try(Connection conn = dataSource.getConnection()) {
+            String sql = "SELECT p.id, p.name, price, currency, p.description, c.name, c.description, c.department, s.name, s.description FROM product p " +
+                    "LEFT JOIN category c ON p.category_id = c.id " +
+                    "LEFT JOIN supplier s on p.supplier_id = s.id " +
+                    "WHERE p.id = ?";
+            PreparedStatement st = conn.prepareStatement(sql);
+            st.setInt(1, id);
+            ResultSet rs = st.executeQuery();
+            if (!rs.next()) {
+                return null;
+            }
+            return fillOutProductForm(rs);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
@@ -54,11 +69,7 @@ public class ProductDaoJdbc implements ProductDao {
             ResultSet rs = conn.createStatement().executeQuery(sql);
             List<Product> result = new ArrayList<>();
             while (rs.next()) { // while result set pointer is positioned before or on last row read authors
-                ProductCategory cat = new ProductCategory(rs.getString(6), rs.getString(7), rs.getString(8));
-                Supplier sup = new Supplier(rs.getString(9), rs.getString(10));
-                Product product = new Product(rs.getString(2), BigDecimal.valueOf(rs.getInt(3)), rs.getString(4), rs.getString(5), cat, sup);
-                product.setId(rs.getInt(1));
-                result.add(product);
+                result.add(fillOutProductForm(rs));
             }
             return result;
         } catch (SQLException e) {
@@ -68,11 +79,49 @@ public class ProductDaoJdbc implements ProductDao {
 
     @Override
     public List<Product> getBy(Supplier supplier) {
-        return data.stream().filter(t -> t.getSupplier().equals(supplier)).collect(Collectors.toList());
+        try (Connection conn = dataSource.getConnection()) {
+            String sql = "SELECT p.id, p.name, price, currency, p.description, c.name, c.description, c.department, s.name, s.description FROM product p " +
+                    "LEFT JOIN category c ON p.category_id = c.id " +
+                    "LEFT JOIN supplier s on p.supplier_id = s.id " +
+                    "WHERE s.id = ?";
+            PreparedStatement st = conn.prepareStatement(sql);
+            st.setInt(1, supplier.getId());
+            ResultSet rs = conn.createStatement().executeQuery(sql);
+            List<Product> result = new ArrayList<>();
+            while (rs.next()) { // while result set pointer is positioned before or on last row read authors
+                result.add(fillOutProductForm(rs));
+            }
+            return result;
+        } catch (SQLException e) {
+            throw new RuntimeException("Error while reading all authors", e);
+        }
     }
 
     @Override
     public List<Product> getBy(ProductCategory productCategory) {
-        return data.stream().filter(t -> t.getProductCategory().equals(productCategory)).collect(Collectors.toList());
+        try (Connection conn = dataSource.getConnection()) {
+            String sql = "SELECT p.id, p.name, price, currency, p.description, c.name, c.description, c.department, s.name, s.description FROM product p " +
+                    "LEFT JOIN category c ON p.category_id = c.id " +
+                    "LEFT JOIN supplier s on p.supplier_id = s.id " +
+                    "WHERE c.id = ?";
+            PreparedStatement st = conn.prepareStatement(sql);
+            st.setInt(1, productCategory.getId());
+            ResultSet rs = conn.createStatement().executeQuery(sql);
+            List<Product> result = new ArrayList<>();
+            while (rs.next()) { // while result set pointer is positioned before or on last row read authors
+                result.add(fillOutProductForm(rs));
+            }
+            return result;
+        } catch (SQLException e) {
+            throw new RuntimeException("Error while reading all authors", e);
+        }
+    }
+
+    private Product fillOutProductForm(ResultSet rs) throws SQLException {
+        ProductCategory cat = new ProductCategory(rs.getString(6), rs.getString(7), rs.getString(8));
+        Supplier sup = new Supplier(rs.getString(9), rs.getString(10));
+        Product product = new Product(rs.getString(2), BigDecimal.valueOf(rs.getInt(3)), rs.getString(4), rs.getString(5), cat, sup);
+        product.setId(rs.getInt(1));
+        return product;
     }
 }
