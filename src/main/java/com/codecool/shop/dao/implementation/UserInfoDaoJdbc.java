5ed7288f36c7ee92b859bf928d997879056225ac
zsomborszego <szego.zsombor@gmail.com>
2021-12-16 11:03:35 +0100

    Add: UserInfoDaoJdbc to hadnle user db querys

---
+++ src/main/java/com/codecool/shop/dao/implementation/UserInfoDaoJdbc.java
@@ -0,0 +1,42 @@
+package com.codecool.shop.dao.implementation;
+
+import com.codecool.shop.dao.UserDao;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserInfoDaoJdbc implements UserDao {
+    private static UserInfoDaoJdbc instance = null;
+    private DataSource dataSource;
+
+    private UserInfoDaoJdbc(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public static UserInfoDaoJdbc getInstance(DataSource dataSource) {
+        if (instance == null) {
+            instance = new UserInfoDaoJdbc(dataSource);
+        }
+        return instance;
+    }
+
+    @Override
+    public boolean getUserDetails(String email, String password) {
+        try (Connection conn = dataSource.getConnection()) {
+            String sql = "SELECT * FROM users WHERE email = ? and password = ?" ;
+            PreparedStatement st = conn.prepareStatement(sql);
+            st.setString(1, email);
+            st.setString(2, password);
+            ResultSet rs = st.executeQuery();
+            if (!rs.next()) {
+                return false;
+            }
+            return true;
+        } catch (SQLException e) {
+            throw new RuntimeException("bd connection is failed");
+        }
+    }
+}
